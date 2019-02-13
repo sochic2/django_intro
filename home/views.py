e063b3fbd72f9dc3db8789cdb232bf3c0a18653a
sochic2 <netzzang12@gmail.com>
2019-02-13 05:19:18 +0000

    05_form data GET POST

--- home/views.py
+++ home/views.py
@@ -20,3 +20,35 @@ def cube(request, number):
     thnum = number **3
     return render(request, 'cube.html', {'number':number, 'thnum':thnum})
     
+def ping(request):
+    return render(request, 'ping.html')
+
+def pong(request):
+    print(request.GET)
+    data = request.GET.get('data')
+    return render(request, 'pong.html', {'data':data})
+
+def user_new(request):
+    return render(request, 'user_new.html')
+    
+def user_create(request):
+    nickname = request.POST.get('nickname')
+    password = request.POST.get('password')
+    return render(request, 'user_create.html', {'password':password, 'nickname':nickname})
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
