9de283262c17ea174efc75614ff0e0723eecf0a7
sochic2 <netzzang12@gmail.com>
2019-02-14 01:21:46 +0000

    07_DTL Practice

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, HttpResponse
 import random
+from datetime import datetime
 # from pprint import pprint
+
 # Create your views here.
 def index(request):
     # print(request)
     # print(type(request))
     # pprint(request.META)
-    return HttpResponse('Welcome to Django !')
+    # return HttpResponse('Welcome to Django !')
+    return render(request, 'index.html')
 
 def dinner(request):
     menus = ['밥', '국', '면', '찌개', '왕밤빵']
@@ -36,7 +39,17 @@ def user_create(request):
     password = request.POST.get('password')
     return render(request, 'user_create.html', {'password':password, 'nickname':nickname})
     
-    
+def template_example(request):
+    my_list = ['짜장면', '탕수육', '짬뽕', '양장피']
+    my_sentence = 'Life is short, you need python'
+    messages = ['apple', 'dog', 'banana', 'cherry', 'melon', 'cat']
+    empty_list = []
+    datetimenow = datetime.now()
+    return render(request, 'template_example.html', 
+                {'my_list':my_list, 'my_sentence' : my_sentence,
+                    'messages' : messages, 'empty_list' : empty_list,
+                    'datetimenow':datetimenow
+                })
     
     
     
